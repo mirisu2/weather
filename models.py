cc55c5f4cacbc6be34ac2e46c3505acbb52c6a04
arty <arty3818@gmail.com>
2019-12-03 15:40:18 +0300

    v1

---
+++ models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, Column, String, DateTime, Integer
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+
+import config
+
+engine = create_engine(f"mysql+pymysql://{config.DB_USER}:{config.DB_PASS}@{config.DB_HOST}/{config.DB_NAME}",
+                       echo=False)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class Weather(Base):
+    __tablename__ = 'yandex_weather'
+    id = Column(INTEGER(unsigned=True), primary_key=True, nullable=False)
+    date = Column(DateTime, server_default=func.now(), nullable=False)
+    pressure = Column(INTEGER(unsigned=True), nullable=False, comment='Давление (в мм рт. ст.)')
+    temp = Column(Integer, nullable=False, comment='Температура (°C)')
+    feels_like = Column(Integer, nullable=False, comment='Ощущаемая температура (°C)')
+    wind_speed = Column(INTEGER(unsigned=True), nullable=False, comment='Скорость ветра (в м/с)')
+    wind_dir = Column(String(3), nullable=False, comment='Направление ветра.')
+    humidity = Column(INTEGER(unsigned=True), nullable=False, comment='Влажность воздуха (в процентах)')
+    obs_time = Column(INTEGER(unsigned=True), nullable=False,
+                      comment='Время замера погодных данных в формате Unixtime.')
+    season = Column(String(6), nullable=False)
+
+    def get_last_weather():
+        return session.execute('SELECT id, pressure, temp, feels_like, wind_speed, wind_dir, humidity, '
+                               'FROM_UNIXTIME(obs_time) AS obs_time FROM yandex_weather ORDER BY id DESC '
+                               'LIMIT 1').fetchone()
+
+
+Base.metadata.create_all(engine)
---
+++ weather.py
@@ -0,0 +1,64 @@
+import logging
+import logging.handlers as handlers
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, render_template
+import requests
+from config import API_KEY, url
+from models import Weather, session
+
+
+logger = logging.getLogger('__weather__')
+logger.setLevel(logging.INFO)
+ch_file = handlers.RotatingFileHandler('weather.log', maxBytes=1000000, backupCount=5)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+ch_file.setFormatter(formatter)
+logger.addHandler(ch_file)
+
+app = Flask(__name__)
+scheduler = BackgroundScheduler()
+
+
+def get_data_from_api():
+    try:
+        logger.info('Making request...')
+        req = requests.get(url, headers={'X-Yandex-API-Key': API_KEY})
+
+        if req.status_code == 200:
+            logger.info(f'req.status_code: {req.status_code}')
+            req_json = req.json()
+            curr = Weather(
+                pressure=req_json['fact']['pressure_mm'],
+                temp=req_json['fact']['temp'],
+                feels_like=req_json['fact']['feels_like'],
+                wind_speed=req_json['fact']['wind_speed'],
+                wind_dir=req_json['fact']['wind_dir'],
+                humidity=req_json['fact']['humidity'],
+                obs_time=req_json['fact']['obs_time'],
+                season=req_json['fact']['season']
+            )
+            session.add(curr)
+            session.commit()
+            if curr.id:
+                logger.info(f'Insert new record ID:{curr.id}')
+            else:
+                logger.error('Cant\'t add new record')
+
+        elif req.status_code == 403:
+            logger.error(f'req.status_code: {req.status_code}')
+
+    except requests.exceptions.ConnectionError as e:
+        logger.error(f'ConnectionError: {e}')
+
+
+scheduler.add_job(get_data_from_api, 'cron', minute=55, max_instances=1)
+scheduler.start()
+
+
+@app.route('/')
+def hello_world():
+    last_record = Weather.get_last_weather()
+    return render_template('index.html', last_record=last_record)
+
+
+logger.info('Starting app...')
+app.run()
