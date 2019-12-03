83742c55900044b85de376894daadc6d35c82790
arty <arty3818@gmail.com>
2019-12-03 14:22:12 +0300

    change config

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ DB_HOST = 'localhost'
 DB_NAME = 'api'
 DB_USER = 'yandex_weather_user'
 DB_PASS = 'Chitaih4'
+
+# широта(LAT) / долгота(LON)
+LAT = 11.225529
+LON = 12.221105
+LANG = 'ru_RU'
+
+# Тариф «Тестовый»
+url = f'https://api.weather.yandex.ru/v1/forecast/?lat={LAT}&lon={LON}&lang=[{LANG}]'
